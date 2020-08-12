deb4ec1ba7d3b6fddefcbfb2402e0636719d10d8
Charles Stevens <charles.stevens@compasses.com>
2020-08-11 22:01:44 -0500

    Make sure that the r-car sound device is selected when attempting to set the DVC (digital volume control)
    Signed-off-by: Charles Stevens <charles.stevens@compasses.com>

--- recipes-multimedia/alsa/audio-init/audio.sh
+++ recipes-multimedia/alsa/audio-init/audio.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-amixer set 'DVC In' 20% >& /dev/null
-amixer set 'DVC Out' 20% >& /dev/null
+amixer -D hw:rcarsound set 'DVC In' 20% >& /dev/null
+amixer -D hw:rcarsound set 'DVC Out' 20% >& /dev/null
